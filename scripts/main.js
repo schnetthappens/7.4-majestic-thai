7a7fbb5ca1a271b403f64f2b5d5f3ba61fa7f931
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-18 14:25:23 -0400

    Fetch and console.log JSON data

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
+import {FoodListCollection} from '/models/foodListCollection';
+import FoodListView from '/views/foodListView';
+
 (function(){
   'use strict';
 
   $(document).ready(function(){
-    $('.content').append(JST.menu());
-    $('.content').append(JST.order());
+    Backbone.history.start();
+
+    var foods = new FoodListCollection();
+      foods.fetch().then(function(){
+        console.log(foods);
+
+      });
+
+    // var foodListView = new FoodListView();
+    //   $('.menu-container').append(foodListView.el);
+
+    // $('.content').append(JST.order());
+
+
   });
 })();
