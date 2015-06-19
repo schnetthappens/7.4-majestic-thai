93110e143f4faf51e6a66f1918b04beeaa0e71ce
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-19 11:32:54 -0400

    Define an Order model that acts like a collection

---
+++ scripts/models/order.js
@@ -0,0 +1,20 @@
+
+import {FoodListCollection} from '/foodListCollection';
+
+export default Backbone.Model.extend({
+
+  initialize: function(){
+    this.foods = new FoodListCollection();
+  },
+
+
+  render: function(){
+
+
+  },
+
+
+
+
+
+});
